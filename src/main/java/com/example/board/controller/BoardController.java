be5f3eda8e8cc65ab819ac1fb7b056d505ef305c
Jung-Taemin <jungtaemin1214@naver.com>
2026-02-19 19:36:47 +0900

    findById까지

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,6 +30,20 @@ public class BoardController {
 
     @GetMapping("/list")
     public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        System.out.println("boardDTOList = " + boardDTOList);
+        return "list";
+    }
 
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        // 조회수 처리
+        boardService.updateHits(id);
+        // 상세내용 가져옴
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        System.out.println("boardDTO = " + boardDTO);
+        return "detail";
     }
 }
