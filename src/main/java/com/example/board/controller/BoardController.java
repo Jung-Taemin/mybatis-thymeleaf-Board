babab914b2ec687f5006b298bb428decf83a45a1
Jung-Taemin <jungtaemin1214@naver.com>
2026-02-19 18:51:27 +0900

    first commit

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,32 @@
+package com.example.board.controller;
+
+import com.example.board.dto.BoardDTO;
+import com.example.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "index";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+
+    }
+}
